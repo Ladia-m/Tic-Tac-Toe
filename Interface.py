82dd9c537117714920d09644b57bdb8c2051d1d8
Ladia-m <ladislav.miko.study@email.cz>
2018-10-26 12:04:05 +0200

    Added grid variable self.playGrid for background image to process in code. Fixed placement more immages (cross, or circle) into one cell and added small infotables.

--- Interface.py
+++ Interface.py
@@ -11,14 +11,14 @@ class Main_window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
-        self.playGrid = [] # X = 1 ; O = 0
+        self.playGrid = [] # player O = 0, player X = 1, empty = 2
         self.lastPlayer = 0
-        self.square_size = 50
+        self.square_size = 25
         self.init_window()
         self.grid(pady=20)
 
     def init_window(self):
-        self.master.title("TIC TAC TOE")
+        self.master.title("Connect five!")
 
         topbar = Menu(self.master)
         self.master.config(menu=topbar)
@@ -57,8 +57,8 @@ class Main_window(Frame):
     def start_game(self, field_size):
 
         def draw_cross(x, y):
-            x = (x - 1) * self.square_size
-            y = (y - 1) * self.square_size
+            x = x * self.square_size
+            y = y * self.square_size
             grid_canvas.create_line(x + 3,
                                     y + 3,
                                     x + self.square_size - 3,
@@ -74,8 +74,8 @@ class Main_window(Frame):
                                     width=3)
 
         def draw_circle(x, y):
-            x = (x - 1) * self.square_size
-            y = (y - 1) * self.square_size
+            x = x * self.square_size
+            y = y * self.square_size
             grid_canvas.create_oval(x + 3,
                                     y + 3,
                                     x + self.square_size - 3,
@@ -83,44 +83,29 @@ class Main_window(Frame):
                                     outline='#ff0000',
                                     width=3)
 
-        def create_hlabel(size):
-            label_width = size * self.square_size
-            hlabel = Canvas(game_window,
-                            width=label_width,
-                            height=self.square_size)
-            for num in range(1, size + 1):
-                x = ((num - 1) * self.square_size) + self.square_size / 2
-                y = self.square_size / 2
-                hlabel.create_text(x, y, text=num)
-            return hlabel
-
-        def create_vlabel(size):
-            abc = ascii_uppercase
-            label_height = size * self.square_size
-            vlabel = Canvas(game_window,
-                            width=self.square_size,
-                            height=label_height)
-            counter = 1
-            while not counter > size:
-                x = self.square_size / 2
-                y = ((counter - 1) * self.square_size) + self.square_size / 2
-                letter = abc[counter - 1]
-                vlabel.create_text(x, y, text=letter)
-                counter += 1
-            return vlabel
-
         def mouse_click(event):
             y, x = event.x, event.y
-            y = y // self.square_size + 1
-            x = x // self.square_size + 1
-            letterx = ascii_uppercase[x]
+            y = y // self.square_size
+            x = x // self.square_size
+            letterx = abc[x]
 
-            if self.lastPlayer == 0:
-                draw_cross(y, x)
-                self.lastPlayer = 1
+            if self.playGrid[x][y] == 2:
+                if self.lastPlayer == 0:
+                    draw_cross(y, x)
+                    self.lastPlayer = 1
+                    self.playGrid[x][y] = 1
+                    top_infotable.configure(text='Player O is on turn.')
+                    bottom_infotable.configure(text="X played cell {}, {}".format(letterx, y + 1))
+                else:
+                    draw_circle(y, x)
+                    self.lastPlayer = 0
+                    self.playGrid[x][y] = 0
+                    top_infotable.configure(text='Player X is on turn.')
+                    bottom_infotable.configure(text="O played cell {}, {}".format(letterx, y + 1))
             else:
-                draw_circle(y, x)
-                self.lastPlayer = 0
+                bottom_infotable.configure(text='Cell is already occupied!')
+
+        abc = ascii_uppercase
 
         game_window = Toplevel(self)
         game_window.title("You are playing size {} x {}".format(field_size, field_size))
@@ -129,6 +114,11 @@ class Main_window(Frame):
         game_window.config(menu=topbar)
         topbar.add_command(label="Restart Game", command=self.client_restart)
 
+        for row in range(0, field_size):
+            columns = []
+            for column in range(0, field_size):
+                columns.append(2)
+            self.playGrid.append(columns)
 
         grid_width = field_size * self.square_size
         grid_height = grid_width
@@ -136,17 +126,44 @@ class Main_window(Frame):
                              width=grid_width,
                              height=grid_height)
         grid_canvas.grid(row=1, column=1, padx=0, pady=0)
-
-        horizontal_label = create_hlabel(field_size)
-        horizontal_label.grid(row=0, column=1)
-        vertical_label = create_vlabel(field_size)
-        vertical_label.grid(row=1, column=0)
-
         grid_canvas.create_rectangle(0, 0, grid_width, grid_height, fill='#ffffff')
         for i in range(1, field_size):
             xy = self.square_size * i
             grid_canvas.create_line(xy, 0, xy, grid_height, fill="#000000")
             grid_canvas.create_line(0, xy, grid_width, xy, fill="#000000")
+
+
+        label_width = field_size * self.square_size
+        horizontal_label = Canvas(game_window,
+                        width=label_width,
+                        height=self.square_size)
+        for num in range(1, field_size + 1):
+            x = ((num - 1) * self.square_size) + self.square_size / 2
+            y = self.square_size / 2
+            horizontal_label.create_text(x, y, text=num)
+        horizontal_label.grid(row=0, column=1)
+
+        label_height = field_size * self.square_size
+        vertical_label = Canvas(game_window,
+                        width=self.square_size,
+                        height=label_height)
+        counter = 1
+        while not counter > field_size:
+            x = self.square_size / 2
+            y = ((counter - 1) * self.square_size) + self.square_size / 2
+            letter = abc[counter - 1]
+            vertical_label.create_text(x, y, text=letter)
+            counter += 1
+        vertical_label.grid(row=1, column=0)
+
+        if self.lastPlayer == 1:
+            player = 'O'
+        else:
+            player = 'X'
+        top_infotable = Label(game_window, text='Player {} is on turn.'.format(player))
+        top_infotable.grid(row=3, column=1)
+        bottom_infotable = Label(game_window)
+        bottom_infotable.grid(row=4, column=1, pady=5)
 
         grid_canvas.bind("<Button-1>", mouse_click)
 
@@ -160,5 +177,5 @@ class Main_window(Frame):
 
 if __name__ == '__main__':
     root = Tk()
-    TTT = Main_window(root)
-    TTT.mainloop()
+    CFive = Main_window(root)
+    CFive.mainloop()
