a8e9dd4977f118148b4f89476e3ac6516c194f1c
Sean Davey <sdavey@redhat.com>
2018-10-27 02:50:32 +0200

    (Endless)
        Hey Miko,  Lots of changes here bro, hope you like the new
    additions, read through the whole list and let me know if you have any
    problems. Looking forward to hear what you think. See you.
    
       * changed title to connect 4
       * new icons
       * added About section
       * Can now press Enter instead of pressing confirm button
       * removed confirm from confirm button (not needed)
       * "waiting for first move"
       * changed to red / yellow circles ( no X's in connect 4)
       * Player 1 + Player2
       * moved both draw functions into one ( can still be improved )
           still has draw "X" and draw "O", didnt change it yet
       * renamed 'abc' =  get better at naming variables pls man :)

--- Interface.py
+++ Interface.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env Python
+#!/usr/bin/env python
 import os
 from tkinter import *
 from string import ascii_uppercase
 import sys
-
+from tkinter import messagebox
 
 class Main_window(Frame):
     counter = 0
@@ -18,28 +18,29 @@ class Main_window(Frame):
         self.grid(pady=20)
 
     def init_window(self):
-        self.master.title("Connect five!")
-
+        self.master.title("Connect four!!")
         topbar = Menu(self.master)
         self.master.config(menu=topbar)
         topbar.add_command(label="Exit", command=self.client_exit)
+        topbar.add_command(label="About", command=self.client_about)
 
-        self.img = PhotoImage(file='data/gifs/img.gif')
+        self.img = PhotoImage(file='data/gifs/connect4')
+
         logo = Label(self.master, image=self.img)
         logo.grid(row=0, column=1, pady=10, padx=10)
 
         entry1label = Label(self.master, text="Enter size (5-10):")
-        entry1label.grid(row=1, column=1, padx=10)
+        entry1label.grid(row=1, column=1, padx=100)
 
         self.entry1 = Entry(self.master)
-        self.entry1.config(width=10)
+        self.entry1.config(width=18)
+        self.entry1.bind('<Return>', lambda e: entry_button.config(self.size_test()))
         self.entry1.grid(row=2, column=1)
 
-        self.confirm_img = PhotoImage(file='data/gifs/1.gif')
-        entry_button = Button(self.master, text="confirm",
-                              padx=2, command=self.size_test)
+        self.confirm_img = PhotoImage(file='data/gifs/ok')
+        entry_button = Button(self.master, padx=4, command=self.size_test)
         entry_button.config(image=self.confirm_img, compound='left')
-        entry_button.grid(row=3, column=1)
+        entry_button.grid(row=5, column=1, rowspan=4)
 
     def size_test(self):
         error_message = Label(self.master, width=35)
@@ -56,56 +57,52 @@ class Main_window(Frame):
 
     def start_game(self, field_size):
 
-        def draw_cross(x, y):
+        def draw_shape(shape, x, y ,magic_number=3):
+            lettery = UPPERCASE_ASCII[y]
             x = x * self.square_size
             y = y * self.square_size
-            grid_canvas.create_line(x + 3,
-                                    y + 3,
-                                    x + self.square_size - 3,
-                                    y + self.square_size - 3,
-                                    fill='#0000ff',
-                                    width=3)
+            self.bottom_infotable = Label(game_window)
+            self.bottom_infotable.grid(row=4, column=1, pady=5)
 
-            grid_canvas.create_line(x + self.square_size - 3,
-                                    y + 3,
-                                    x + 3,
-                                    y + self.square_size - 3,
-                                    fill='#0000ff',
-                                    width=3)
+            if shape == "X":
+                grid_canvas.create_oval(x + magic_number,
+                                        y + magic_number,
+                                        x + self.square_size - magic_number,
+                                        y + self.square_size - magic_number,
+                                        outline='black', fill='yellow',
+                                        width=magic_number)
+                self.lastPlayer = 0
+                self.top_infotable.configure(text='Player 1 is on turn.')
+                self.bottom_infotable.configure(text="Player 2 played cell {}, {}".format(lettery, x // 25 + 1))
 
-        def draw_circle(x, y):
-            x = x * self.square_size
-            y = y * self.square_size
-            grid_canvas.create_oval(x + 3,
-                                    y + 3,
-                                    x + self.square_size - 3,
-                                    y + self.square_size - 3,
-                                    outline='#ff0000',
-                                    width=3)
+            elif shape == "O":
+                grid_canvas.create_oval(x + magic_number,
+                                        y + magic_number,
+                                        x + self.square_size - magic_number,
+                                        y + self.square_size - magic_number,
+                                        outline='black', fill='red',
+                                        width=magic_number)
+                self.lastPlayer = 1
+                self.top_infotable.configure(text='Player 2 is on turn.')
+                self.bottom_infotable.configure(text="Player 1 played cell {}, {}".format(lettery, x // 25 + 1))
 
         def mouse_click(event):
             y, x = event.x, event.y
             y = y // self.square_size
             x = x // self.square_size
-            letterx = abc[x]
+
 
             if self.playGrid[x][y] == 2:
-                if self.lastPlayer == 0:
-                    draw_cross(y, x)
-                    self.lastPlayer = 1
+                if self.lastPlayer == 1:       # 1 == x
+                    draw_shape("X", y, x)
                     self.playGrid[x][y] = 1
-                    top_infotable.configure(text='Player O is on turn.')
-                    bottom_infotable.configure(text="X played cell {}, {}".format(letterx, y + 1))
                 else:
-                    draw_circle(y, x)
-                    self.lastPlayer = 0
+                    draw_shape("O", y, x)      # 0 == O
                     self.playGrid[x][y] = 0
-                    top_infotable.configure(text='Player X is on turn.')
-                    bottom_infotable.configure(text="O played cell {}, {}".format(letterx, y + 1))
             else:
-                bottom_infotable.configure(text='Cell is already occupied!')
+                self.bottom_infotable.configure(text='Cell is already occupied!')   # 2 == occupied
 
-        abc = ascii_uppercase
+        UPPERCASE_ASCII = ascii_uppercase
 
         game_window = Toplevel(self)
         game_window.title("You are playing size {} x {}".format(field_size, field_size))
@@ -113,6 +110,8 @@ class Main_window(Frame):
         topbar = Menu(game_window)
         game_window.config(menu=topbar)
         topbar.add_command(label="Restart Game", command=self.client_restart)
+        self.top_infotable = Label(game_window, text='Waiting for your first move:')
+        self.top_infotable.grid(row=11, column=1)
 
         for row in range(0, field_size):
             columns = []
@@ -151,19 +150,10 @@ class Main_window(Frame):
         while not counter > field_size:
             x = self.square_size / 2
             y = ((counter - 1) * self.square_size) + self.square_size / 2
-            letter = abc[counter - 1]
+            letter = UPPERCASE_ASCII[counter - 1]
             vertical_label.create_text(x, y, text=letter)
             counter += 1
         vertical_label.grid(row=1, column=0)
-
-        if self.lastPlayer == 1:
-            player = 'O'
-        else:
-            player = 'X'
-        top_infotable = Label(game_window, text='Player {} is on turn.'.format(player))
-        top_infotable.grid(row=3, column=1)
-        bottom_infotable = Label(game_window)
-        bottom_infotable.grid(row=4, column=1, pady=5)
 
         grid_canvas.bind("<Button-1>", mouse_click)
 
@@ -173,6 +163,9 @@ class Main_window(Frame):
     def client_restart(self):
         python = sys.executable
         os.execl(python, python, *sys.argv)
+
+    def client_about(self):
+        messagebox.showinfo("About", "Connect4 - Miko 2018\n Version 0.5\n")
 
 
 if __name__ == '__main__':
