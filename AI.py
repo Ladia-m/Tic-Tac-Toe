063261c8a74299aecbe0e261134bad2e4525965d
Ladia-m <ladislav.miko.study@email.cz>
2019-02-25 11:56:35 +0100

    Improved defense method again and finished first version of offense method.

--- AI.py
+++ AI.py
@@ -9,6 +9,7 @@ class AI:
         self.play_grid = []
         self.rival_moves = []
         self.my_moves = []
+        self.default_score = 10
         self.directions = ['tt', 'tr', 'rr', 'br', 'bb', 'bl', 'll', 'tl']
         for column in range(0, self.grid_size):
             rows = []
@@ -32,7 +33,8 @@ class AI:
             y = coordinates[1]
             self.play_grid[x][y] = 1
             self.rival_moves.append(coordinates)
-            self.set_defense_score()
+            defeat_in = self.set_defense_score()
+            self.set_offense_score(defeat_in)
             top_score = [0]
             for column in range(0, self.grid_size):
                 for row in range(0, self.grid_size):
@@ -56,7 +58,6 @@ class AI:
         return x, y
 
     def set_defense_score(self):
-        default_score = 10
         rival_lines = []
         rival_singles = []
         defeat_in = 5
@@ -94,16 +95,16 @@ class AI:
                     if ([x, y] not in self.my_moves) and (self.within_grid([x, y])):
                         if defeat_in > 4:
                             defeat_in = 4
-                        self.score_grid[x][y] += default_score
+                        self.score_grid[x][y] += self.default_score
                         # if there are rival singles in 2 cells distance, add extra score
                         line = self.cells_on_line('empty&rival', direction, [x, y], 4, True)
                         if len(line) == 4 and line[1] in self.rival_moves:
-                            if self.score_grid[x][y] < 2 * default_score:
-                                self.score_grid[x][y] += default_score
+                            if self.score_grid[x][y] < 2 * self.default_score:
+                                self.score_grid[x][y] += self.default_score
                             if defeat_in > 3:
                                 defeat_in = 3
                         # maybe create case for three singles situation X_X_X here
-        # where the magic happens:
+        # where the magic happens (add score to ends of rival lines):
         for line in rival_lines:
             direction_1 = self.check_direction(line[-1], line[0])
             x, y = self.cellxy(line[0], direction_1, 1)
@@ -117,23 +118,23 @@ class AI:
                         continue
                     variable_score = 0
                     distance = 0
-                    if len(line_extension_1) + len(line) < 5 or len(line_extension_2) + len(line) < 5:
-                        variable_score -= default_score
-                    if len(line_extension) + len(line) < 5:
-                        variable_score -= default_score
-                    if len(line) == 3 and len(line_extension_1) > 0 and len(line_extension_2) > 0:
-                        variable_score += 4 * default_score
-                    if len(line) == 4:
-                        variable_score += 5 * default_score
                     counter = 0
                     for cell in line_extension:
                         if cell in self.rival_moves:
                             counter += 1
+                    if len(line_extension_1) + len(line) < 5 or len(line_extension_2) + len(line) < 5:
+                        variable_score -= self.default_score
+                    if len(line_extension) + len(line) < 5:
+                        variable_score -= self.default_score
+                    if len(line) == 3 and len(line_extension_1) > 0 and len(line_extension_2) > 0:
+                        variable_score += 3 * self.default_score
+                    if len(line) + counter == 4:
+                        variable_score += 5 * self.default_score
                     if len(line_extension) > 1 and line_extension[1] in self.rival_moves:
                             x, y = line_extension[0]
-                            self.score_grid[x][y] += (len(line) + counter) * default_score
+                            self.score_grid[x][y] += (len(line) + counter) * self.default_score
                     for x, y in line_extension:
-                        self.score_grid[x][y] += default_score * (len(line) - distance + 1) + variable_score
+                        self.score_grid[x][y] += self.default_score * (len(line) - distance + 1) + variable_score
                         if self.score_grid[x][y] < 0:
                             self.score_grid[x][y] = 0
                         distance += 1
@@ -149,21 +150,19 @@ class AI:
                         cells_around.append([x, y])
             for x, y in cells_around:
                 if x < self.grid_size and y < self.grid_size:
-                    if self.score_grid[x][y] < 2 * default_score:
-                        self.score_grid[x][y] += default_score
+                    if self.score_grid[x][y] < 2 * self.default_score:
+                        self.score_grid[x][y] += self.default_score
             # if two parallel lines with 1 cell gap, add extra score between them
             for direction in self.directions:
                 for cell in line:
                     chkline = self.cells_on_line('empty&rival', direction, cell, 5, True)
                     if len(chkline) == 5 and chkline[1] not in self.rival_moves and chkline[2] in self.rival_moves:
                         x, y = chkline[1]
-                        if self.score_grid[x][y] < 3 * default_score:
-                            self.score_grid[x][y] = 3 * default_score
+                        if self.score_grid[x][y] < 3 * self.default_score:
+                            self.score_grid[x][y] = 3 * self.default_score
         return defeat_in
 
     def set_offense_score(self, defeat_in):
-        win_in = 5
-        default_score = 5
         my_lines = []
         my_singles = []
         for cell in self.my_moves:
@@ -194,7 +193,65 @@ class AI:
             else:
                 my_singles.append(center_cell)
 
-
+        for line in my_lines:
+            direction_1 = self.check_direction(line[-1], line[0])
+            x, y = self.cellxy(line[0], direction_1, 1)
+            line_extension_1 = self.cells_on_line('empty&me', direction_1, [x, y], 5 - len(line), True)
+            direction_2 = self.check_direction(line[0], line[-1])
+            x, y = self.cellxy(line[-1], direction_2, 1)
+            line_extension_2 = self.cells_on_line('empty&me', direction_2, [x, y], 5 - len(line), True)
+            if len(line) + len(line_extension_1) + len(line_extension_2) < 5:
+                continue
+            for line_extension in [line_extension_1, line_extension_2]:
+                if len(line_extension) == 0:
+                    continue
+                counter = 0
+                for cell in line_extension:
+                    if cell in self.my_moves:
+                        counter += 1
+                win_in = 5 - len(line) - counter
+                if win_in == 1:
+                    x, y = line_extension[0]
+                    self.score_grid[x][y] += 20 * self.default_score
+                    return
+                # Modify score based on count of turns needed to win and to be defeated
+#                offense_score = self.default_score + (defeat_in - win_in + 1) * (self.default_score / 5)
+                if defeat_in >= win_in:
+                    offense_score = int(self.default_score * 1.2)
+                elif defeat_in < win_in:
+                    offense_score = int(self.default_score / 5)
+                else:
+                    offense_score = self.default_score
+                variable_score = 0
+                if len(line_extension_1) < 1 or len(line_extension_2) < 1:
+                    variable_score -= offense_score
+                if len(line_extension) + len(line) < 5:
+                    variable_score -= offense_score
+                if len(line) == 3 and len(line_extension_1) > 1 and len(line_extension_2) > 1:
+                    variable_score += 3 * offense_score
+                if len(line_extension) > 1 and line_extension[1] in self.rival_moves:
+                    x, y = line_extension[0]
+                    self.score_grid[x][y] += offense_score
+                distance = 0
+                for x, y in line_extension:
+                    self.score_grid[x][y] += offense_score * (len(line) - distance + 1) + variable_score
+                    if self.score_grid[x][y] < 0:
+                        self.score_grid[x][y] = 0
+                    distance += 1
+        if defeat_in < 4:
+            offense_score = int(self.default_score / 5)
+        else:
+            offense_score = int(self.default_score * 1.1)
+        for cell in my_singles:
+            for i in range(0, 4):
+                line_extension_1 = self.cells_on_line('empty&me', self.directions[i], cell, 5, True)
+                line_extension_2 = self.cells_on_line('empty&me', self.directions[i + 4], cell, 5, True)
+                if len(line_extension_1) > 1 and len(line_extension_2) > 1:
+                    if len(line_extension_1) + len(line_extension_2) > 5: #>5 because both extensions includes 1st cell
+                        x, y = line_extension_1[1]
+                        self.score_grid[x][y] += offense_score
+                        x, y = line_extension_2[1]
+                        self.score_grid[x][y] += offense_score
 
     def check_around(self, center_cell, competitor):
         cells_around = {}
@@ -275,30 +332,6 @@ class AI:
             counter += 1
             if counter == length:
                 break
-        if option == 'rival':
-            return_line = []
-            for cell in whole_line:
-                if cell in self.rival_moves:
-                    return_line.append(cell)
-                elif stop:
-                    break
-            return return_line
-        if option == 'me':
-            return_line = []
-            for cell in whole_line:
-                if cell in self.my_moves:
-                    return_line.append(cell)
-                elif stop:
-                    break
-            return return_line
-        if option == 'empty':
-            return_line = []
-            for cell in whole_line:
-                if cell not in self.my_moves and cell not in self.rival_moves:
-                    return_line.append(cell)
-                elif stop:
-                    break
-            return return_line
         if option == 'empty&rival':
             return_line = []
             for cell in whole_line:
@@ -307,5 +340,11 @@ class AI:
                 elif stop:
                     break
             return return_line
-        if option == 'all':
-            return whole_line
+        if option == 'empty&me':
+            return_line = []
+            for cell in whole_line:
+                if cell not in self.rival_moves:
+                    return_line.append(cell)
+                elif stop:
+                    break
+            return return_line
